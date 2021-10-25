2471d621607ef8e28e83826698059e9971707800
Arslan <ali.arslan319@gmail.com>
2021-10-25 16:58:39 -0400

    create constructor for creating items, setting initial state with hardcoded items in ItemController

--- app.js
+++ app.js
@@ -2,7 +2,22 @@
 
 // Item Controller - IIFE
 const ItemController = (function(){
-
+    // creating Item constructor to create items 
+    const Item = function(id, name, calories){
+        this.id = id, 
+        this.name = name, 
+        this.calories = calories
+    }
+    // creating data structure / initial state (mimic react state)
+    const state = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200}, 
+            {id: 1, name: 'Ice cream sandwich', calories: 400}, 
+            {id: 2, name: 'Fried eggs', calories: 300}
+        ], 
+        currentItem: null, 
+        calories: 0
+    }
 })()
 
 // UI Controller - IIFE
